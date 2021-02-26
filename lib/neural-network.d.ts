68f142908ea3d293175b2b704a1809f42a4bccf6
MathleteDev <mathletedev@gmail.com>
2021-02-25 20:27:38 -0800

    typescript build

--- lib/neural-network.d.ts
+++ lib/neural-network.d.ts
@@ -9,6 +9,5 @@ export default class NeuralNetwork {
     constructor(sizes: number[], lr?: number, activation?: string);
     predict(inputs: number[]): number[];
     train(inputs: number[], targets: number[]): void;
-    printData(): void;
 }
 //# sourceMappingURL=neural-network.d.ts.map
